9d5e99b0eee2b9857582fe3b316137f28754a9df
Matt Ehrnschwender <matthewe2020@gmail.com>
2020-06-14 12:59:37 -0400

    Built

--- get-ip.go
+++ get-ip.go
@@ -2,8 +2,35 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "io/ioutil"
+    "log"
+    "encoding/json"
 )
 
+type data struct {
+    IP string `json:"ip"`
+}
+
 func main() {
-    fmt.Println("Hello, world.")
+
+    url := "https://api.ipify.org?format=json"
+    var response data
+
+    resp, err := http.Get(url)
+
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    json.Unmarshal([]byte(body), &response)
+    fmt.Println(response.IP)
+
 }
