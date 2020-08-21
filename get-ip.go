b592aec597f3a7e6223e209cb4bc77c613e6051d
Matt Ehrnschwender <matthewe2020@gmail.com>
2020-08-21 12:52:19 -0400

    Added ipv6 support

--- get-ip.go
+++ get-ip.go
@@ -1,36 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "log"
-    "encoding/json"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 type data struct {
-    IP string `json:"ip"`
+	IP string `json:"ip"`
 }
 
 func main() {
 
-    url := "https://api.ipify.org?format=json"
-    var response data
+	var url string
+	protocol := flag.Int("protocol", 4, "Format to return the IP address either 6 for IPv6 or 4 for IPv4")
+	flag.Parse()
 
-    resp, err := http.Get(url)
+	switch *protocol {
+	case 4:
+		url = "https://api.ipify.org?format=json"
+	case 6:
+		url = "https://api6.ipify.org?format=json"
+	default:
+		fmt.Println("Invalid protocol. Must be either 6 or 4.")
+		os.Exit(1)
+	}
+	var response data
 
-    if err != nil {
-        log.Fatalln(err)
-    }
+	resp, err := http.Get(url)
 
-    defer resp.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	defer resp.Body.Close()
 
-    json.Unmarshal([]byte(body), &response)
-    fmt.Println(response.IP)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	json.Unmarshal([]byte(body), &response)
+	fmt.Println(response.IP)
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/MEhrn00/get-ip
 
-go 1.14
+go 1.15
